f9887c0767a9354771c3f9eff2b31bf67ef408a4
Gustavo <gustavoguimaraes@gmail.com>
2014-02-22 19:25:56 -0500

    Turn urn CSV into JSON

---
+++ static/csvToJson.js
@@ -0,0 +1,77 @@
+function CSVToArray(strData, strDelimiter) {
+    // Check to see if the delimiter is defined. If not,
+    // then default to comma.
+    strDelimiter = (strDelimiter || ",");
+    // Create a regular expression to parse the CSV values.
+    var objPattern = new RegExp((
+    // Delimiters.
+    "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+    // Quoted fields.
+    "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+    // Standard fields.
+    "([^\"\\" + strDelimiter + "\\r\\n]*))"), "gi");
+    // Create an array to hold our data. Give the array
+    // a default empty first row.
+    var arrData = [[]];
+    // Create an array to hold our individual pattern
+    // matching groups.
+    var arrMatches = null;
+    // Keep looping over the regular expression matches
+    // until we can no longer find a match.
+    while (arrMatches = objPattern.exec(strData)) {
+        // Get the delimiter that was found.
+        var strMatchedDelimiter = arrMatches[1];
+        // Check to see if the given delimiter has a length
+        // (is not the start of string) and if it matches
+        // field delimiter. If id does not, then we know
+        // that this delimiter is a row delimiter.
+        if (strMatchedDelimiter.length && (strMatchedDelimiter != strDelimiter)) {
+            // Since we have reached a new row of data,
+            // add an empty row to our data array.
+            arrData.push([]);
+        }
+        // Now that we have our delimiter out of the way,
+        // let's check to see which kind of value we
+        // captured (quoted or unquoted).
+        if (arrMatches[2]) {
+            // We found a quoted value. When we capture
+            // this value, unescape any double quotes.
+            var strMatchedValue = arrMatches[2].replace(
+            new RegExp("\"\"", "g"), "\"");
+        } else {
+            // We found a non-quoted value.
+            var strMatchedValue = arrMatches[3];
+        }
+        // Now that we have our value string, let's add
+        // it to the data array.
+        arrData[arrData.length - 1].push(strMatchedValue);
+    }
+    // Return the parsed data.
+    return (arrData);
+}
+
+function toJSON(csv) {
+    var array =  CSVToArray(csv);
+    var objArray = [];
+    for(var i = 1; i < array.length; i++){
+        objArray.push(array[i])
+        
+    }
+    data = {} 
+    for(var i =0; i< objArray.length; i++){
+        data[objArray[i][0]] = objArray[i][1];
+
+    }
+    
+    var json = JSON.stringify(data);
+    var solution = []
+    
+    solution.push(json.trim());
+    return solution.sort();
+}
+
+$("#conversion").click(function() {
+    var csv = $("#csv").val();
+    var json = toJSON(csv);
+    $("#json").val(json);
+});
